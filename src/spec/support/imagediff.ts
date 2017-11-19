bc352cf63c7a92a951da7a23c4a152aebab120c3
eonarheim <erik.onarheim@gmail.com>
2017-11-19 12:12:56 -0600

    WIP matcher

--- src/spec/support/imagediff.ts
+++ src/spec/support/imagediff.ts
@@ -4,19 +4,63 @@ let toImageDataFromCanvas = function(canvas: HTMLCanvasElement): ImageData {
    let ctx = canvas.getContext('2d');
    let {width, height} = canvas;
    return ctx.getImageData(0, 0, width, height);
-}
+};
 
 let toImageDataFromPath = function(pathToImage: string) {
    let img = new Image();
+   img.src = pathToImage;
+   let loaded = false;
    img.addEventListener('load', () => {
-
+      loaded = true;
    });
+
+   while (!loaded) {
+      // spin until loaded
+   }
+
+   let canvas = new HTMLCanvasElement();
+   let ctx = canvas.getContext('2d');
+   ctx.drawImage(img, img.naturalWidth, img.naturalHeight);
+   return ctx.getImageData(0, 0, img.naturalWidth, img.naturalHeight);
+};
+
+interface ImageComparison {
+   actualImageData: ImageData;
+   expectedImageData: ImageData;
+   diffImageData: ImageData;
+   percentDifferent: number;
 }
 
-let toImageDataFromImage = function(image: HTMLImageElement) {
-   
-}
+function compareImageData(imageActualData: ImageData, imageExpectedData: ImageData): ImageComparison {
+   let widthDiff = imageActualData.width - imageExpectedData.width;
+   let heightDiff = imageActualData.height - imageExpectedData.height;
 
+   let result: ImageComparison = {
+      actualImageData: imageActualData,
+      expectedImageData: imageExpectedData,
+      percentDifferent: 100
+   }
+   if (widthDiff === 0 && heightDiff === 0) {
+      let width = imageActualData.width;
+      let height = imageActualData.height;
+      for (let i = 0; i < imageActualData.width; i++) {
+         for (let j = 0; j < imageActualData.height; j++) {
+            let pixelIndex = i + j * width;
+            let actualPixelR = imageActualData[pixelIndex + 0];
+            let actualPixelG = imageActualData[pixelIndex + 1];
+            let actualPixelB = imageActualData[pixelIndex + 2];
+            let actualPixelA = imageActualData[pixelIndex + 3];
+
+            let expectedPixelR = imageExpectedData[pixelIndex + 0];
+            let expectedPixelG = imageExpectedData[pixelIndex + 1];
+            let expectedPixelB = imageExpectedData[pixelIndex + 2];
+            let expectedPixelA = imageExpectedData[pixelIndex + 3];
+         }
+      }
+   }
+
+   return null;
+}
 
 let customMatcher = {
    toImageEqual: function(util, customEqualityTesters) {
